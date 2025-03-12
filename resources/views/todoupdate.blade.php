f5d7b56296e47e4661884e6a937a9d9b05f11194
JonsenMarcell <you@example.com>
2025-03-12 10:42:38 +0700

    hehebuoy

--- resources/views/todoupdate.blade.php
+++ resources/views/todoupdate.blade.php
@@ -19,6 +19,7 @@
                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
                                    readonly>
                         </div>
+                        <!--Tes github-->
 
                         <!-- Nama Pekerjaan (Readonly) -->
                         <div class="col-span-2">
